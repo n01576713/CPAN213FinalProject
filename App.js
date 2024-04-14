b6a4aca97e4e7e95422d2257b2195348f5656999
n01576713 <n01576713@humber.ca>
2024-04-13 20:34:18 -0400

    to browse genres
    to browse games with a picker(this is temp and will be ajusted)

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
+import BrowseScreen from './screens/BrowseScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +13,9 @@ function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Cart" component={CartScreen} />
+        <Tab.Screen name="Browse" component={BrowseScreen} />
         <Tab.Screen name="Favorites" component={FavoritesScreen} />
+        <Tab.Screen name="Cart" component={CartScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );

7f0fd3aab73220be379e324c17c0f921ee685337
IHMLXDKW <164953951+IHMLXDKW@users.noreply.github.com>
2024-04-19 22:25:01 -0400

    Tab Icons
    Navigation tab has icons.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
@@ -11,7 +12,23 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? require('./images/Home.png') : require('./images/Home.png');
+          } else if (route.name === 'Browse') {
+            iconName = focused ? require('./images/Browse.png') : require('./images/Browse.png');
+          } else if (route.name === 'Favorites') {
+            iconName = focused ? require('./images/Favorite.png') : require('./images/Favorite.png');
+          } else if (route.name === 'Cart') {
+            iconName = focused ? require('./images/Cart.png') : require('./images/Cart.png');
+          }
+          return <Image source={iconName} style={{ width: 25, height: 25 }} />;
+        },
+      })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Browse" component={BrowseScreen} />
         <Tab.Screen name="Favorites" component={FavoritesScreen} />
